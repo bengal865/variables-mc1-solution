429565dcc4f29efaef5d4da240363341cf3cee93
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-04 09:21:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ num2 = float(input('Please enter a second number: (Example: 3)\n')
 # Process
 # Square and cube the first and second numbers respectively
 num_squared = num1 ** 2
-num_cubed = num2 *** 3
+num_cubed = num2 ** 3
 
 # Output
 # Display the square and cube of the numbers
