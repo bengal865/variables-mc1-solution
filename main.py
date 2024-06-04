7f6dec80f424cbd18b5a879e06e7fa37c1942c6f
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-04 09:21:33 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Variables MC Solution
+
+# Input
+num1 = float(input('Please enter your first number: (Example: 5)\n')
+num2 = float(input('Please enter a second number: (Example: 3)\n')
+
+# Process
+# Square and cube the first and second numbers respectively
+num_squared = num1 ** 2
+num_cubed = num2 *** 3
+
+# Output
+# Display the square and cube of the numbers
+print(f'{num1} squared is: {num_squared}')
+print(f'{num2} cubed is: {num_cubed}')
+print('Thank you for using my script!')
